4f7ee2551357f894735f3531f369e2af68ae6d7e
antoninCabane <antonin.cabane@gmail.com>
2020-12-07 11:34:28 +0100

    Création de la route GET /notes/eleve/[:id_eleve]" - version pour Guillaume v2

---
+++ src/DataFixtures/EtudiantFixture.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Etudiant;
+use App\Entity\Personne;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class EtudiantFixture extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        $personne = new Personne();
+        $personne->setPrenom("Antonin");
+        $personne->setNom("CABANE");
+        $personne->setAdresse("479 Avenue des euzières 34190 Brissac");
+        $personne->setEmail("antonin.cabane@gmail.com");
+        $personne->setNumeroTel("0750214383");
+
+
+        $etudiant = new Etudiant();
+        $etudiant->setPersonne($personne);
+        $etudiant->setIsAlternant(true);
+
+        $manager->persist($personne);
+        $manager->persist($etudiant);
+        $manager->flush();
+    }
+}
