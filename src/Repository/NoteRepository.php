9c6ffc5eedb909c2644c9f6d27372d69ab4283bb
antoninCabane <antonin.cabane@gmail.com>
2020-12-07 11:34:01 +0100

    Création de la route GET /notes/eleve/[:id_eleve]" - version pour Guillaume

--- src/Repository/NoteRepository.php
+++ src/Repository/NoteRepository.php
@@ -6,6 +6,7 @@ use App\Entity\Etudiant;
 use App\Entity\Matiere;
 use App\Entity\Note;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\DBAL\Exception;
 use Doctrine\ORM\EntityManager;
 use Doctrine\Persistence\ManagerRegistry;
 
@@ -71,6 +72,62 @@ class NoteRepository extends ServiceEntityRepository
         }
 
         return false;
+    }
+
+    public function getAllNotesBySemestre(int $idEtudiant, int $idSemestre)
+    {
+
+        $sql = "SELECT S.id as \"IDSEMESTRE\", S.nom as \"NOMSEMESTRE\", MO.id as \"IDMODULE\", MO.nom as \"NOMMODULE\", MA.id as \"IDMATIERE\", MA.nom as \"NOMMATIERE\", N.note, MA.coefficient".
+                " FROM Semestre S".
+                " JOIN module MO ON MO.semestre_id=S.id".
+                " JOIN matiere MA ON MA.module_id=MO.id".
+                " JOIN note N ON N.matiere_id=MA.id".
+                " JOIN etudiant E ON N.etudiant_id=E.id".
+                " WHERE E.id = $idEtudiant AND S.id = $idSemestre".
+                " GROUP BY S.id";
+
+        $stmt = null;
+        try {
+            $stmt = $this->getEntityManager()->getConnection()->prepare($sql);
+        } catch (Exception $e) {
+            return null;
+        }
+        try {
+            $stmt->execute([]);
+        } catch (\Doctrine\DBAL\Driver\Exception $e) {
+            return null;
+        }
+
+        $result = $stmt->fetchAll();
+
+        $resultFormatted = [];
+
+       # "IDSEMESTRE": "1",
+       # "NOMSEMESTRE": "Semestre 1",
+       # "IDMODULE": "1",
+       # "NOMMODULE": "8n4BfmIzFJ",
+       # "IDMATIERE": "1",
+       # "NOMMATIERE": "oqsQatqRL6VQ",
+       # "note": "19",#"coefficient": "1"
+
+        $modules = [];
+
+        foreach ($result as $elem) {
+            $idModule = $elem["IDMODULE"];
+            $idMatiere = $elem["IDMATIERE"];
+            $nomModule = $elem["NOMMODULE"];
+            $nomMatiere = $elem["NOMMATIERE"];
+            $note = $elem["NOTE"];
+
+            foreach($modules as $module){
+                if()
+            }
+
+        }
+
+
+
+        return $result;
     }
 
     // /**
