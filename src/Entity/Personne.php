09e0e3bb2087973d44d59f379ed4924250057298
Guillaume de Maleprade <guillaume.de-maleprade@mines-ales.org>
2021-02-12 15:55:20 +0100

    Gestion accents email

--- src/Entity/Personne.php
+++ src/Entity/Personne.php
@@ -153,8 +153,27 @@ class Personne implements UserInterface
 
     public function generateEmail(bool $isEtudiant = true){
         if(!isset($this->email)){
-            $this->email = strtolower($this->prenom) . "." . strtolower(str_replace(" ", "-", $this->nom)) . "@mines-ales" . ($isEtudiant ? ".org" : ".fr");
+            $nom = $this->normalize($this->nom);
+            $nom = strtolower(str_replace(" ", "-", $nom));
+            $prenom = $this->normalize($this->prenom);
+            $prenom = strtolower($prenom);
+            $this->email = $prenom . "." . $nom . "@mines-ales" . ($isEtudiant ? ".org" : ".fr");
         }
+    }
+
+    function normalize ($string) {
+        $table = array(
+            'Š'=>'S', 'š'=>'s', 'Đ'=>'Dj', 'đ'=>'dj', 'Ž'=>'Z', 'ž'=>'z', 'Č'=>'C', 'č'=>'c', 'Ć'=>'C', 'ć'=>'c',
+            'À'=>'A', 'Á'=>'A', 'Â'=>'A', 'Ã'=>'A', 'Ä'=>'A', 'Å'=>'A', 'Æ'=>'A', 'Ç'=>'C', 'È'=>'E', 'É'=>'E',
+            'Ê'=>'E', 'Ë'=>'E', 'Ì'=>'I', 'Í'=>'I', 'Î'=>'I', 'Ï'=>'I', 'Ñ'=>'N', 'Ò'=>'O', 'Ó'=>'O', 'Ô'=>'O',
+            'Õ'=>'O', 'Ö'=>'O', 'Ø'=>'O', 'Ù'=>'U', 'Ú'=>'U', 'Û'=>'U', 'Ü'=>'U', 'Ý'=>'Y', 'Þ'=>'B', 'ß'=>'Ss',
+            'à'=>'a', 'á'=>'a', 'â'=>'a', 'ã'=>'a', 'ä'=>'a', 'å'=>'a', 'æ'=>'a', 'ç'=>'c', 'è'=>'e', 'é'=>'e',
+            'ê'=>'e', 'ë'=>'e', 'ì'=>'i', 'í'=>'i', 'î'=>'i', 'ï'=>'i', 'ð'=>'o', 'ñ'=>'n', 'ò'=>'o', 'ó'=>'o',
+            'ô'=>'o', 'õ'=>'o', 'ö'=>'o', 'ø'=>'o', 'ù'=>'u', 'ú'=>'u', 'û'=>'u', 'ý'=>'y', 'þ'=>'b',
+            'ÿ'=>'y', 'Ŕ'=>'R', 'ŕ'=>'r',
+        );
+
+        return strtr($string, $table);
     }
 
     public function getArray()
