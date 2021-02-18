42c4f82b3a32ca899faf718a38a553b343a2760b
Guillaume de Maleprade <guillaume.de-maleprade@mines-ales.org>
2021-02-18 16:24:23 +0100

    GET promo by formation + GET promo by respo

--- src/Entity/Formation.php
+++ src/Entity/Formation.php
@@ -18,7 +18,11 @@ class Formation
 {
     /**
      * @OA\Property(type="integer")
-     * @Groups({"get_etudiant","get_formation", "get_promotion"})
+     * @Groups({
+     *     "get_etudiant",
+     *     "get_formation",
+     *     "get_promotion"
+     * })
      * @ORM\Id
      * @ORM\GeneratedValue
      * @ORM\Column(type="integer")
@@ -27,7 +31,11 @@ class Formation
 
     /**
      * @OA\Property(type="string")
-     * @Groups({"get_etudiant","get_formation", "get_promotion"})
+     * @Groups({
+     *     "get_etudiant",
+     *     "get_formation",
+     *     "get_promotion"
+     * })
      * @ORM\Column(type="string", length=255)
      */
     private $nom;
@@ -51,7 +59,9 @@ class Formation
 
     /**
      * @ORM\ManyToOne(targetEntity=Personne::class, inversedBy="formations")
-     * @Groups({"get_formation"})
+     * @Groups({
+     *     "get_formation"
+     * })
      * @ORM\JoinColumn(nullable=false)
      */
     private $responsable;
@@ -59,7 +69,10 @@ class Formation
     /**
      * @OA\Property(type="boolean")
      * @ORM\Column(type="boolean")
-     * @Groups("get_etudiant")
+     * @Groups({
+     *     "get_etudiant",
+     *     "get_promotion"
+     * })
      */
     private $isAlternance;
 
