5f861d7737092950faa2b3bd2581bf4555140dc4
antoninCabane <antonin.cabane@gmail.com>
2021-02-26 09:58:32 +0100

    Creation de la route GET /personnes/eligiblent/intervenants

--- src/Controller/PersonneController.php
+++ src/Controller/PersonneController.php
@@ -3,7 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\Personne;
+use App\Repository\AssistantRepository;
+use App\Repository\IntervenantRepository;
 use App\Repository\PersonneRepository;
+use App\Repository\ResponsableRepository;
 use App\Serializers\GenericSerializer;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -94,6 +97,39 @@ class PersonneController extends AbstractController
         return new JsonResponse("Non trouvé", Response::HTTP_NOT_FOUND);
 
 
+    }
+
+    /**
+     * @OA\Get(
+     *      tags={"Personnes"},
+     *      path="/personnes/eligiblent/intervenants",
+     *      @OA\Response(
+     *          response="200",
+     *          @OA\JsonContent(ref="#/components/schemas/Personne")
+     *      )
+     * )
+     * @Route("/personnes/eligiblent/intervenants", name="get_personnes_eligibles_responsable", methods={ "GET" } )
+     * @param PersonneRepository $personneRepository
+     * @param AssistantRepository $assistantRepository
+     * @param IntervenantRepository $intervenantRepository
+     * @param ResponsableRepository $responsableRepository
+     * @return Response
+     */
+    public function getPersonnesEligiblentIntervenants(PersonneRepository $personneRepository,AssistantRepository $assistantRepository, IntervenantRepository $intervenantRepository, ResponsableRepository $responsableRepository):Response {
+
+        $repoResponse = $personneRepository->getPersonnesEligiblentIntervenants($personneRepository,$assistantRepository,$responsableRepository,$intervenantRepository);
+
+        switch ($repoResponse["status"]){
+            case 200:
+                $json = GenericSerializer::serializeJson($repoResponse['data'], ['groups'=>'get_personne']);
+                return new JsonResponse($json,Response::HTTP_OK);
+                break;
+            case 500:
+                return new JsonResponse($repoResponse["error"],Response::HTTP_INTERNAL_SERVER_ERROR);
+                break;
+            default:
+                return new JsonResponse(Response::HTTP_NOT_FOUND);
+        }
     }
 
 
