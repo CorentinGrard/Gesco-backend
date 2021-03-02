19c4b8c74d66dac5af6cc73139981378fe9dcaf0
Guillaume de Maleprade <guillaume.de-maleprade@mines-ales.org>
2021-03-02 10:39:55 +0100

    Roles CRUD Intervenant

--- src/Controller/IntervenantController.php
+++ src/Controller/IntervenantController.php
@@ -7,6 +7,7 @@ use App\Entity\Matiere;
 use App\Repository\IntervenantRepository;
 use App\Serializers\GenericSerializer;
 use Doctrine\ORM\EntityManagerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -29,6 +30,7 @@ class IntervenantController extends AbstractController
      * @Route("/intervenants", name="list_intervenants", methods={"GET"})
      * @param IntervenantRepository $intervenantRepository
      * @return Response
+     * @Security("is_granted('ROLE_ADMIN') or is_granted('ROLE_ASSISTANT')")
      */
     public function list(IntervenantRepository $intervenantRepository): Response
     {
@@ -57,6 +59,7 @@ class IntervenantController extends AbstractController
      * @Route("/intervenants/{id}", name="get_intervenant", methods={"GET"})
      * @param Intervenant|null $intervenant
      * @return Response
+     * @Security("is_granted('ROLE_ADMIN') or is_granted('ROLE_ASSISTANT')")
      */
     public function read(Intervenant $intervenant = null): Response
     {
@@ -88,6 +91,7 @@ class IntervenantController extends AbstractController
      * @Route("/matieres/{id}/intervenants", name="intervenant_by_matiere", methods={"GET"})
      * @param Matiere|null $matiere
      * @return Response
+     * @Security("is_granted('ROLE_ADMIN') or is_granted('ROLE_ASSISTANT')")
      */
     public function intervenantsParMatiere(Matiere $matiere = null): Response
     {
@@ -117,6 +121,7 @@ class IntervenantController extends AbstractController
      * @param Intervenant|null $intervenant
      * @param EntityManagerInterface $em
      * @return Response
+     * @Security("is_granted('ROLE_ADMIN') or is_granted('ROLE_ASSISTANT')")
      */
     public function deleteIntervenant(EntityManagerInterface $em, Intervenant $intervenant = null):Response
     {
@@ -151,6 +156,7 @@ class IntervenantController extends AbstractController
      * @param Request $request
      * @param IntervenantRepository $repository
      * @return Response
+     * @Security("is_granted('ROLE_ADMIN') or is_granted('ROLE_ASSISTANT')")
      */
     public function addIntervenant(Request $request, IntervenantRepository $repository):Response
     {
@@ -214,6 +220,7 @@ class IntervenantController extends AbstractController
      * @param IntervenantRepository $repository
      * @param Intervenant|null $intervenant
      * @return Response
+     * @Security("is_granted('ROLE_ADMIN') or is_granted('ROLE_ASSISTANT')")
      */
     public function updateIntervenant(Request $request, IntervenantRepository $repository, Intervenant $intervenant = null):Response
     {
